557a34fd5af7a4d00e6adc89fd23be2320423721
_efilym <efilym@mail.ru>
2020-04-25 08:21:23 +0300

    lees4

--- l4_task2.py
+++ l4_task2.py
@@ -3,3 +3,18 @@
 Подсказка: элементы, удовлетворяющие условию, оформить в виде списка.
 Для формирования списка использовать генератор."""
 
+my_list = [5, 1, 16, 3, 15, 60]
+print(my_list)
+
+# new_list = [itm for itm in my_list]
+# print(new_list)
+
+for itm in my_list:
+    for itm_2 in my_list:
+        # if itm > itm_2:
+        #     print(itm)
+        #     break
+        if itm_2 or itm:
+            print(itm_2)
+            break
+
